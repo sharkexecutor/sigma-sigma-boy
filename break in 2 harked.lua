7c3e28473a77fceefe20fc8ee3c03d012d6e8436
Getsaturn <karimxain1@gmail.com>
2025-02-11 08:33:51 +0200

    Update break in 2 harked.lua

--- break in 2 harked.lua
+++ break in 2 harked.lua
@@ -1,5 +1,5 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {Text="Script Modified By __venommcat__", Title="Harked Reborn v2"})
-setclipboard("__venommcat__")
+game:GetService("StarterGui"):SetCore("SendNotification", {Text="Script Modified By __zainxain__", Title="Kiko Reborn v2"})
+setclipboard("zainxaino")
 game:GetService("StarterGui"):SetCore("SendNotification", {Text="Copied username if you wanna add and chat with me <3", Title="Copied"})
 
 
